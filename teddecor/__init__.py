3d451a339f14277527575ce3290cf5ad2765ac78
Zachary Boehm <zboehm@menard-inc.com>
2022-08-04 16:30:31 -0500

    Bumped TEDDecor version for TEDTest

--- teddecor/__init__.py
+++ teddecor/__init__.py
@@ -5,6 +5,6 @@ This is a easy to use library with testing, documentation, and docstring example
 Dive in with minimal effort and get great results.
 """
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 from . import UnitTest
 from .TED import TED
